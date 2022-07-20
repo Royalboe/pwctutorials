599251bbd24564548aa5842b94fcb260c3d03dbe
Royalboe <auomidejohnson@gmail.com>
2022-07-20 10:06:03 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// Data Types
+let name = "Ayomide"; // String
+let age = 39; // Number
+let z = null; // Null
+let y = undefined; // Undefined
+let isMarried = true; // Boolean
+let favColor = ["green", "blue", "purple"]
+
+console.log(age);
+console.log(name);
+console.log(typeof y);
+console.log(typeof ismarried);
+
+// Strings
+let s = "Hello, World";
+console.log(s.toUpperCase());
+console.log(s.length);
+
+
+let m = "I am";
+let n = "I go to PWC Bootcamp";
+let p = "I love to learn JavaScript";
