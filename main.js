2c5bb07307074b6f10052e8ca4ab43ce33aef5f0
Royalboe <auomidejohnson@gmail.com>
2022-07-20 11:03:47 +0100

    Training

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let age = 39; // Number
 let z = null; // Null
 let y = undefined; // Undefined
 let isMarried = true; // Boolean
-let favColor = ["green", "blue", "purple"]
+let favColor = ["green", "blue", "purple"]; // Arrays
+let fruits = ["Apple", "Banana", "Grapes", "Oranges", "Pineapple"]; // Arrays
 
 console.log(age);
 console.log(name);
@@ -16,7 +17,17 @@ let s = "Hello, World";
 console.log(s.toUpperCase());
 console.log(s.length);
 
+console.warn("This is a warning");
+console.error('This is an error');
+console.info("This is an Info");
+console.debug('For debugging');
+console.assert();
+
 
 let m = "I am";
 let n = "I go to PWC Bootcamp";
 let p = "I love to learn JavaScript";
+
+console.log(m +" " + name + " " + "and " + p);
+
+console.log(`${m} ${name} and ${p}`)
